faa9353b90bebbcf14aceaeebb119870480aeb77
patilyash8113 <yashpatil8113@gmail.com>
2025-12-31 11:03:29 +0530

    Add files via upload

---
+++ 03_variables/main.py
@@ -0,0 +1,16 @@
+age=14 #integer
+name="yash" #string
+marks=20.2 #float
+
+
+age2=53
+print("age2")
+print(type(age))
+
+name="yash"
+print(name)
+print(type(name))
+
+cgpa=50.5
+print(cgpa)
+print(type(cgpa))
